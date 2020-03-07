a2e0cacaef7942c4ac43a2418b27272cf3e3bc55
Surathkal <36732414+Surathkal@users.noreply.github.com>
2020-03-07 18:19:38 -0500

    Initial Solution Setup

---
+++ MyShopNag/MyShopNag.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyShopNag.WebUI.Startup))]
+namespace MyShopNag.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
